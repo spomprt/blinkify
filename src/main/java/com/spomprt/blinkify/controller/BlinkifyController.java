9e262ee4cd802615bd7260d1bedf76b1b11d83bf
khafizullinba <khafizullinba@pochtabank.ru>
2023-11-29 23:59:16 +0300

    initial commit

---
+++ src/main/java/com/spomprt/blinkify/controller/BlinkifyController.java
@@ -0,0 +1,25 @@
+package com.spomprt.blinkify.controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/blinkify/api/v1")
+public class BlinkifyController {
+
+    @GetMapping
+    public void redirect(
+            HttpServletResponse response
+    ) {
+        try {
+            response.sendRedirect("https://vk.com/feed");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
